e1c472119678739a89a3c23e27302dd6ccff91db
Arturo Fernandez <arturo.fernandez@leafgroup.com>
2021-06-24 15:48:13 -0700

    Using async/await

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const k8s = require('@kubernetes/client-node');
 
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
+const run = async () => {
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault();
 
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-k8sApi.listNamespace()
-  .then((res) => {
-    for(let ns of res.body.items) {
-      console.log(ns.metadata?.name);
+
+    const namespaces = await k8sApi.listNamespace();
+    for (let ns of namespaces.body.items) {
+        console.log(ns.metadata.name);
     }
-});
+}
+
+run();
